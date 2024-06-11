4097ffe85ec26f3aedb617738a5399fb69d85ec5
“Seetaram <ssrungaram2@gmail.com>
2024-06-10 21:49:24 -0400

    maven file

---
+++ MyClassJenkins.java
@@ -0,0 +1,8 @@
+import java.lang.String;
+import java.lang.System;
+
+public class MyClassJenkins {
+    public static void main(String args[]) {
+        System.out.println("Welcome to Jenkins");
+    }
+}
